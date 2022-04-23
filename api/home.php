5631291b0169e30a9e0951085120fbea3c2a1a5b
geanderson062 <geanderson062@outlook.com>
2022-04-23 16:30:27 -0300

    correcao do titulo

--- api/home.php
+++ api/home.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Sistema De Relatórios11</title>
+    <title>Sistema De Relatórios</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
 
